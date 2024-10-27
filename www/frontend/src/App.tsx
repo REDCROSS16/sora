9853a621e92bc91cceb027d37688fba86e56a8a8
AlexRedCross <belkill@mail.ru>
2024-10-27 22:54:45 +0300

    update es-lint + button + input

--- www/frontend/src/App.tsx
+++ www/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Button from './components/Button/Button.tsx';
 import React, {useEffect, useState, MouseEvent} from 'react';
+import {Input} from './components/Input/Input.tsx';
 
 function App() {
 
@@ -10,16 +11,16 @@ function App() {
 	}, []);
 
 	const addCounter = (e: MouseEvent) => {
-		setCounter('asd');  
-
-		console.log(e);
+		console.log('hello');
 	};
 
 	return (
 		<>
-			<Button appearance='asda' onClick={addCounter}>
+			<Button appearance='small' onClick={addCounter}>
                 hello
 			</Button>
+			<Button appearance='big' children={'test text'}/>
+			<Input placeholder='Email'/>
 		</>
 	);
 }
